4035cf1d77b19ee386b5c3ef39eb909f9bb9dcba
ST10506089 <henryviedge@gmail.com>
2026-05-15 09:44:03 +0200

    hey moss, run this

---
+++ src/crazy/Crazy.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crazy;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Amelia_laptop
+ */
+public class Crazy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        while (true) {
+            JOptionPane.showMessageDialog(null, "Crazy? I was Crazy once. They looked me in a room, a rubber room, a rubber room with rats, and rats make me crazy", null,0);
+        }
+        
+    }
+    
+}
